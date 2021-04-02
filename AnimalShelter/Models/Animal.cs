874127116915efa2fc44287e30e5692d0d010612
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-04-02 10:21:51 -0700

    add animal model

---
+++ AnimalShelter/Models/Animal.cs
@@ -0,0 +1,13 @@
+namespace AnimalShelter.Models
+{
+    public class Animal
+    {
+        public int AnimalId { get; set; }
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public DateTime Admitted { get; set; }
+
+    }
+}
