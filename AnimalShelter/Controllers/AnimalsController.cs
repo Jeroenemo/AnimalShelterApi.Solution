f8d944bc1b5c00a3f32aad3f1491111071241023
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-04-02 13:18:06 -0700

    add get all route

---
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AnimalShelter.Models;
+
+namespace AnimalShelter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
+    {
+        private readonly AnimalShelterContext _db;
+
+        public AnimalsController(AnimalShelterContext db)
+        {
+            _db = db;
+        }
+
+        // GET: api/Animals
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Animal>>> Get(string species, string gender, string name)
+        {
+            var query = _db.Animals.AsQueryable();
+
+            if (species != null)
+            {
+                query = query.Where(entry => entry.Species == species);
+            }
+
+            if (gender != null)
+            {
+                query = query.Where(entry => entry.Gender == gender);
+            }
+
+            if (name != null)
+            {
+                query = query.Where(entry => entry.Name == name);
+            }
+
+            return await query.ToListAsync();
+        }
+    }
+}
