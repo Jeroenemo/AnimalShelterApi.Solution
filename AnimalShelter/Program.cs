225c889e4e4b3c21591d6e58e9392d18ca5a3967
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-04-02 10:51:55 -0700

    fix namespace

--- AnimalShelter/Program.cs
+++ AnimalShelter/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Animal
+namespace AnimalShelter
 {
     public class Program
     {
