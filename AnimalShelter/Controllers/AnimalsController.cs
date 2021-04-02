dcde986b1550b6ccce5bb8af0b5c41d3781cbb1c
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-04-02 13:21:12 -0700

    add put route

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -55,5 +55,34 @@ namespace AnimalShelter.Controllers
 
             return animal;
         }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Animal animal)
+        {
+            if (id != animal.AnimalId)
+            {
+                return BadRequest();
+            }
+
+            _db.Entry(animal).State = EntityState.Modified;
+
+            try
+            {
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AnimalExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
     }
 }
