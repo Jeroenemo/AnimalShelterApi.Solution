c9f3232c265ffd11dc903b374e66ef6bca773c66
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-04-04 12:00:58 -0700

    add speciesid

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -6,9 +6,9 @@ namespace AnimalShelter.Models
     {
         public int AnimalId { get; set; }
         public string Name { get; set; }
-        public string Species { get; set; }
         public int Age { get; set; }
         public string Gender { get; set; }
         public DateTime Admitted { get; set; }
+        public int SpeciesId { get; set; }
     }
 }
